138a677947460b0a417dced180d5393b17fc5130
sardorbek-0515 <tursunovsardorbek76@gmail.com>
2026-07-19 23:40:27 +0900

    feat: create homePage Redux configuration

--- src/app/store.ts
+++ src/app/store.ts
@@ -3,14 +3,15 @@ import { configureStore, ThunkAction, Action } from "@reduxjs/toolkit";
 //import ProductsPageReducer from "./screens/productsPage/slice";
 //import OrdersPageReducer from "./screens/ordersPage/slice";
 import reduxLogger from "redux-logger";
+import HomePageReducer from "./screens/homePage/slice";
 
 export const store = configureStore({
   middleware: (getDefaultMiddleware) =>
-    // @ts-ignore
+
     getDefaultMiddleware().concat(reduxLogger),
   reducer: {
     dummy: (state = {}) => state,
-    //homePage: HomePageReducer,
+    homePage: HomePageReducer,
     //productsPage: ProductsPageReducer,
     //ordersPage: OrdersPageReducer,
   },
